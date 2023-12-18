0303cbba9d059b1091435a203c6832ae88b632fe
Aigerim Nassanova <210103044@stu.sdu.edu.kz>
2023-12-18 18:25:06 +0600

    profile fragment fully done with design

--- app/src/main/java/com/example/final_project_android/ProfileFragment.kt
+++ app/src/main/java/com/example/final_project_android/ProfileFragment.kt
@@ -1,7 +1,46 @@
 package com.example.final_project_android
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.final_project_android.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
+class ProfileFragment: Fragment(R.layout.fragment_profile) {
 
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var user: FirebaseAuth
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        user = FirebaseAuth.getInstance()
+
+        if (user.currentUser != null){
+            user.currentUser?.let {
+                binding.textViewUser.text = it.email
+            }
+        }
+
+        binding.buttonSignOut.setOnClickListener{
+            user.signOut()
+            startActivity(
+                Intent(requireContext(), LoginActivity::class.java)
+            )
+
+            requireActivity().finish()
+        }
+    }
 }
